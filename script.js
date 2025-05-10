d4bf46fe65cc4ce9278f6072cd83423cbd3a9f78
sruja16 <srujansrujan0343@gmail.com>
2025-05-10 19:57:57 +0530

    first

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+
+// Functionality for Download CV button 
+//document.querySelector('.download-btn').addEventListener('click', function() { alert('Download CV functionality can be implemented here.'); });
